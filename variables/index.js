48d1fc0394c79e1c8f4354b0dc9d2bf048410d00
Alexandra <alexandrapham03@gmail.com>
2020-09-23 11:30:54 +0200

    feat(Premiers exos JS): First JS exercices

---
+++ variables/index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+//Exercice 1
+ 
+ // My digital School Forever
+
+ //Exercice 2
+
+ /* My Digital School 
+ forever 
+ every day */
+
+ //Exercice 3
+
+var string = '42'; 
+var number = 42; 
+var float = 42.000; 
+var boolean = true; 
+var array = [42]; 
+var date = '42'; 
+var objet = {
+     age : 42
+     }; ;
+var toto = null;
+
+ console.log(string, number, float, boolean, array, date, objet, toto);
+
+ //Exercice 4
+
+ var my42count = "quarante-deux";
+ var my42countLength = my42count.length;
+ console.log(my42countLength);
+
+//Exercice 5
+
+//Exercice 6
+
+var myArray42 = ['q','u','a','r', 'a', 'n', 't', 'e', '-', 'd', 'e', 'u', 'x'];
+
+//Exercice 7
+
+var myArray42Len = myArray42.length;
+console.log(myArray42Len);
+
+//Exercice 8
+var monMot = 'q'+'u'+'a'+ 'r'+ 'a'+ 'n'+ 't'+ 'e'+ '-'+ 'd'+ 'e'+ 'u'+ 'x';
+var maPhrase = 'La grande réponse sur la vie, l’univers et le reste !' + ' ' + monMot;
+console.log(maPhrase);
+
+//Exercice 9
+var rand = Math.random();
