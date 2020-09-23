a0b28496b1affca9f82845eaec5d881c1c63c3d9
Alexandra <alexandrapham03@gmail.com>
2020-09-23 21:24:42 +0200

    feat(final exercice conditions): Update Final Exercices Conditions JS

--- conditions/index.js
+++ conditions/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 //Exercice 1
-
 var age = prompt("Quel âge avez-vous ?");
 console.log(age);
 
@@ -15,8 +14,8 @@ if (age >= 42) {
   alert("Entrez une valeur valable");
 }
  
-//Exercice 2
 
+//Exercice 2
 var rand = Math.floor(Math.random() * (31));
 console.log(rand);
 if (rand = 0 || rand <= 10) {
@@ -60,8 +59,8 @@ switch (new Date().getDay()) {
 
 console.log(day);
 
-//Exercice 4
 
+//Exercice 4
 var histoire = prompt("Choisissez votre fin d'histoire : 1 : Heureuse, 2 : Triste, 3 : Morale");
 
 if (histoire == 1) {
@@ -75,5 +74,42 @@ if (histoire == 1) {
 }
 
 
-// Exercice 6
+// Exercice 5
+var number = prompt("Entrez un nombre pair");
+alert(isPair(number));
 
+function isPair(number){
+  if(number%2==0){
+    return "OK";
+  }
+  return number + " is not pair";
+}
+
+
+// Exercice 6
+var ternaire = ternaire != undefined ? "cette variable n'existe pas" : 42;
+
+
+// Exercice 7
+var family = prompt("Ecrivez un nombre");
+
+switch(Math.trunc(family/10)){
+  case 0:
+    alert("Ce chiffre fait partie de la famille des 0");
+    break;
+  case 1:
+    alert("Ce chiffre fait partie de la famille des 10");
+    break;
+  case 2:
+    alert("Ce chiffre fait partie de la famille des 20");
+    break;
+  case 3:
+    alert("Ce chiffre fait partie de la famille des 30");
+    break;
+  case 4:
+    alert("Ce chiffre fait partie de la famille des 40");
+    break;
+  default:
+    alert("Ce chiffre n'a pas de famille");
+    break;
+}
